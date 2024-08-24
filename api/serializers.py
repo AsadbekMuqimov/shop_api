36022588da3254c77665bf732984cdb532c66f55
AsadbekMuqimov <muqimovasadbek04@gmail.com>
2024-08-24 14:04:36 +0500

    salom

--- api/serializers.py
+++ api/serializers.py
@@ -66,4 +66,4 @@ class WishListSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = '__all__'
